d73e3478f2f24958b92f09e74415f23fb84c901d
retardedmonkeygaming <mohammadharib721@gmail.com>
2026-07-25 15:59:18 +0300

    refactor: Update GPIO pin assignments for clarity and consistency in hardware interfacing

--- hardware/pins.py
+++ hardware/pins.py
@@ -1,17 +1,22 @@
-"""Centralized hardware GPIO pin definitions."""
+"""GPIO Pin Assignments for Raspberry Pi Hardware Interfacing."""
+import board
 
-# LCD Configuration (HD44780 Parallel 4-bit)
-LCD_RS = 22
-LCD_E = 17
-LCD_D4 = 25
-LCD_D5 = 24
-LCD_D6 = 23
-LCD_D7 = 18
+# LCD 16x2 Pin Assignments (HD44780 Parallel Wiring)
+LCD_RS = board.D22      # Register Select
+LCD_E = board.D17       # Enable Pin (LCD_E)
+LCD_D4 = board.D25      # Data Pin 4
+LCD_D5 = board.D24      # Data Pin 5
+LCD_D6 = board.D23      # Data Pin 6
+LCD_D7 = board.D18      # Data Pin 7
 
 LCD_COLUMNS = 16
 LCD_ROWS = 2
 
-# Sensors & Actuators
-DHT_PIN = 4
-TOUCH_PIN = 27
-BUZZER_PIN = 2
+# Touch Sensor Input Pin
+TOUCH_PIN = 27          # BCM GPIO Pin 27 for Touch Input
+
+# Buzzer Pin
+BUZZER_PIN = 12         # BCM GPIO Pin 12 for Buzzer
+
+# DHT11 Sensor Pin
+DHT_PIN = 4             # BCM GPIO Pin 4 for DHT11 Sensor
